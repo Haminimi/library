367958d76088c8c7eb8ca2e32e5ef9c07a87fea5
Hamza Mujinovic <mujinovichamza@gmail.com>
2023-10-27 17:02:28 +0200

    Create main displayBook function
     Created a function, "displayBook," to render book cards in the UI
     based on the books in the library array. Generated the DOM structure
     for the book cards. Added logic to display the appropriate icon and
     color for the "switchButton" element based on the book's read status.
     Implemented logic to check if a book is already in the list of favorite
      books and, if not, added it to the list with appropriate formatting.
      After successfully rendering a book card and handling its read and
      favorite status, the book is added to the "displayedBooks" array to
      prevent duplicates.

--- app.js
+++ app.js
@@ -38,3 +38,93 @@ function addBookToLibrary(book) {
 }}
 addBookToLibrary(theHobbit);
 addBookToLibrary(theLOTR);
+
+
+function displayBook(array) {
+    array.forEach(book => {
+        if (displayedBooks.includes(book)) {
+            return;
+        } else if (!displayedBooks.includes(book)) { 
+
+        //Create DOM structure for every card
+        const newCard = document.createElement('div');
+        newCard.classList.add('card');
+        cards.appendChild(newCard);
+
+        const cardMain = document.createElement('div');
+        cardMain.classList.add('card-main');
+        newCard.appendChild(cardMain);
+
+        const title = document.createElement('p');
+        title.classList.add('card-title');
+        title.textContent = `${book.title}`;
+        cardMain.appendChild(title);
+
+        const author = document.createElement('p');
+        if (book.author === '') {
+            author.textContent = '';
+        } else {
+            author.textContent = `by ${book.author}`;
+            cardMain.appendChild(author);  
+        }
+
+        const year = document.createElement('p');
+        year.textContent = `${book.year}`;
+        cardMain.appendChild(year);
+
+        const readDiv = document.createElement('div');
+        readDiv.classList.add('read-div')
+        const read = document.createElement('p');
+        read.textContent = `${book.read}`;
+        const switchButton = document.createElement('span');
+        switchButton.classList.add('switch-button', 'material-symbols-outlined');
+        readDiv.appendChild(read);
+        readDiv.appendChild(switchButton);
+        cardMain.appendChild(readDiv);
+
+        const cardButtons = document.createElement('div');
+        cardButtons.classList.add('card-buttons');
+        newCard.appendChild(cardButtons);
+
+        const favoriteButton = document.createElement('span');
+        favoriteButton.classList.add('favorite-button', 'material-symbols-outlined');
+        favoriteButton.textContent = 'favorite';
+        cardButtons.appendChild(favoriteButton);
+
+        const deleteButton = document.createElement('span');
+        deleteButton.classList.add('delete-button', 'material-symbols-outlined');
+        deleteButton.textContent = 'delete';
+        cardButtons.appendChild(deleteButton);
+
+        if (read.textContent === 'Read') {
+            switchButton.textContent = 'toggle_on';
+            switchButton.style.color = '#2BAE66';
+        } else if (read.textContent === 'Not read') {
+            switchButton.textContent = 'toggle_off';
+            switchButton.style.color = '#323232';
+        }
+
+        const alreadyInFavorites = favoriteBooks.some(item => {
+            return book.title === item.title && book.author === item.author && book.year === item.year;
+        })
+        if (alreadyInFavorites) {
+            if (book.favorite === 'Yes') {
+                return;
+            }
+        } else if (!alreadyInFavorites) {
+            if (book.favorite === 'Yes') {
+                const favoriteBook = document.createElement('p');
+                if (book.author === '') {
+                    favoriteBook.textContent = `${book.title}`;
+                } else {
+                    favoriteBook.textContent = `${book.title} by ${book.author}`;
+                }
+                favoriteBooksList.appendChild(favoriteBook);
+                favoriteButton.classList.add('favorite-button-red');
+                favoriteBooks.push(book);
+            } else if (book.favorite === 'No') {
+                const favoriteBook = document.createElement('p');
+                favoriteBooksList.appendChild(favoriteBook);
+        }}
+
+        displayedBooks.push(book);
